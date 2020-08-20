69a93de9133fdaf4e36e6668773a9e99e0b9c5d7
diivya <divya.yadav3@wipro.com>
2020-08-20 19:49:53 +0530

    Initial Commit

---
+++ sfg-pet-data/src/main/java/model/Owner.java
@@ -0,0 +1,4 @@
+package model;
+
+public class Owner extends Person {
+}
--- sfg-pet-web/src/main/java/sfgpetclinicdivya/SfgPetClinicDivyaApplication.java
+++ sfg-pet-web/src/main/java/sfgpetclinicdivya/SfgPetClinicDivyaApplication.java
@@ -1,4 +1,4 @@
-package com.example.sfgpetclinicdivya;
+package sfgpetclinicdivya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
