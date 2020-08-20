d8415a2c0ddc6a412d282eb1d3fda342342c5e44
diivya <divya.yadav3@wipro.com>
2020-08-20 11:48:08 +0530

    Initial Commit

---
+++ src/main/java/com/example/sfgpetclinicdivya/SfgPetClinicDivyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinicdivya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicDivyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicDivyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sfgpetclinicdivya/SfgPetClinicDivyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfgpetclinicdivya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicDivyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
